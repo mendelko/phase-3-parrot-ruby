0623fbcb3adf4be980fbbe711b40f44dbae8748c
Mendel <mkonikov123@gmail.com>
2021-08-27 18:44:17 -0400

    commit tree 18b6848bdb35e005dfff904e7b76614f06158451

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+# def parrot(sound = "Squawk!")
+#     puts sound
+#     sound
+# end
+
+def parrot(phrase = "Squawk!")
+    sound = phrase
+    puts sound
+    sound
+end
